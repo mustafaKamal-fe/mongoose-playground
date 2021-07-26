788b2d5edf436bfbabc7809e5a44646d6196f4c2
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-27 01:45:21 +0300

    person model

--- src/models/person.ts
+++ src/models/person.ts
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
+import defaultSchemConfig from '../utils/mongooseSchemaConfig';
 
-const PersonSchema = new mongoose.Schema({
-	name: String,
-	age: Date,
-});
+const PersonSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		age: { type: Date, required: true },
+	},
+	defaultSchemConfig({ withId: true })
+);
 
 const model = mongoose.models.person || mongoose.model('Person', PersonSchema);
 
