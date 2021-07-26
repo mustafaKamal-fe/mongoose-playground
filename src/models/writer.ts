b6c02538b532ff6395c96db684c58b279adcead1
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-27 01:46:04 +0300

    added optimized schema config

--- src/models/writer.ts
+++ src/models/writer.ts
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
+import defaultSchemConfig from '../utils/mongooseSchemaConfig';
 
-const WriterSchema = new mongoose.Schema({
-	identity: { type: mongoose.Schema.Types.ObjectId, ref: 'Person' },
-	books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
-});
+const WriterSchema = new mongoose.Schema(
+	{
+		identity: { type: mongoose.Schema.Types.ObjectId, ref: 'Person' },
+		books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
+	},
+	defaultSchemConfig({ withId: true })
+);
 
 const model = mongoose.models.person || mongoose.model('Writer', WriterSchema);
 
