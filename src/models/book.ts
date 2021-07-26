247d94d2937f5b9b2ef619b9d7ed572f3380c0bc
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-27 01:44:58 +0300

    Book model with two refs to person and bookshop models

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,14 +1,23 @@
 import mongoose from 'mongoose';
+import defaultSchemConfig from '../utils/mongooseSchemaConfig';
 
-const BookSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-	pDate: { type: Date, required: true },
-	writer: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Person',
-		required: true,
+const BookSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		pDate: { type: Date, required: true },
+		writer: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Person',
+			required: true,
+		},
+		bookshop: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Bookshop',
+			required: true,
+		},
 	},
-});
+	defaultSchemConfig({ withId: true })
+);
 
 const model = mongoose.models.book || mongoose.model('Book', BookSchema);
 
