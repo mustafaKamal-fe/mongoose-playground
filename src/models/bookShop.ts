b1220d8a73408c2f5eab04d30cbd96132a2a5730
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-27 01:45:09 +0300

    bookshop model

---
+++ src/models/bookShop.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import defaultSchemConfig from '../utils/mongooseSchemaConfig';
+
+const BookShopSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true },
+		location: { type: String, required: true },
+		website: { type: String },
+	},
+	defaultSchemConfig({ withId: true })
+);
+
+const model =
+	mongoose.models.person || mongoose.model('Bookshop', BookShopSchema);
+
+export default model;
