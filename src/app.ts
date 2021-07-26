fcbf653a01e41b9c43fe289819f9ca081bd34f61
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-27 02:58:03 +0300

    Query-conditions

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import errorHandler from './utils/errorHandler';
 import connectMongoose from './utils/connectMongoose';
@@ -12,23 +12,25 @@ app.use(bodyParser.json());
 // connect to DB (default connection)
 app.use(connectMongoose);
 
-app.post('/person', library.addPerson, (_req: Request, res: Response) => {
-	res.status(201);
-	res.end();
-});
-app.get('/person', library.getPerson);
+// app.post('/person', library.addPerson, (_req: Request, res: Response) => {
+// 	res.status(201);
+// 	res.end();
+// });
+// app.get('/person', library.getPerson);
 
-app.post('/book', library.addBook, (_req: Request, res: Response) => {
-	res.status(201);
-	res.end();
-});
+// app.post('/book', library.addBook, (_req: Request, res: Response) => {
+// 	res.status(201);
+// 	res.end();
+// });
 
-app.get('/book', library.getBook);
+// app.get('/book', library.getBook);
 
-app.post('/bookshop', library.addBookShop, (_req: Request, res: Response) => {
-	res.status(201);
-	res.end();
-});
+// app.post('/bookshop', library.addBookShop, (_req: Request, res: Response) => {
+// 	res.status(201);
+// 	res.end();
+// });
+
+app.get('/books', library.filterBooks);
 
 // error handler
 app.use(errorHandler);
--- src/middlewares/library.ts
+++ src/middlewares/library.ts
@@ -108,10 +108,46 @@ const addBookShop: RequestHandler = async (
 	await doc.save();
 	next();
 };
+
+const filterBooks: RequestHandler = async (req: Request, res: Response) => {
+	const { location, website } = req.query;
+	const match: any = { location, website };
+	const query: any = {};
+
+	// conditional query object
+	for (const par in match) {
+		if (par) query[par] = match[par];
+	}
+
+	let books = await Book.find()
+		.populate({
+			path: 'bookshop',
+			match: query,
+		})
+		.lean()
+		.exec();
+
+	res.status(200);
+	res.json({
+		books,
+		explaination: `
+	Suppose document A has "ref" to document B. Generally, Document B can get populated like usual.
+	However, If we would like to filter any query to Document A, like when you normally make a query 
+	based on some condition such as "A.find({...conditions})", BUT, now we query using the population
+	path as the filter like "A.find().populate({path: 'B', match: {...conditions}})". Then, we can not do that
+	and THERE IS NO way to do so. If this query matches any, it will return the matched documents along with the
+	populated data, offcourse. However, if non match, the populated path will be null (or []) and the rest is
+	returned as if there is no query filter all together. see [https://mongoosejs.com/docs/populate.html#query-conditions]
+
+	`,
+	});
+	res.end();
+};
 export default {
 	getBook,
 	getPerson,
 	addPerson,
 	addBook,
 	addBookShop,
+	filterBooks,
 };
