e0fe03724a03602d48c3ceb9458da8c2ca8ca17b
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-31 21:48:05 +0300

    commented code

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import connectMongoose from './utils/connectMongoose';
 import filterQueryLibrary from './middlewares/filters-query/library';
 import basicPopulationLibrary from './middlewares/basic/library';
 import limitedPopulationLibrary from './middlewares/limited/library';
+import nestedLibrary from './middlewares/nested/index';
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,11 @@ app.use(bodyParser.json());
 // connect to DB (default connection)
 app.use(connectMongoose);
 
+/**
+ * Basic population
+ */
+
+// PERSON
 app.post(
 	'/person',
 	basicPopulationLibrary.addPerson,
@@ -24,6 +30,7 @@ app.post(
 );
 app.get('/person', basicPopulationLibrary.getPerson);
 
+// BOOK
 app.post(
 	'/book',
 	basicPopulationLibrary.addBook,
@@ -32,9 +39,19 @@ app.post(
 		res.end();
 	}
 );
-
 app.get('/book', basicPopulationLibrary.getBook);
 
+// WRITER
+app.get('/writer', basicPopulationLibrary.getWriter);
+app.post(
+	'/writer',
+	basicPopulationLibrary.addWriter,
+	(_req: Request, res: Response) => {
+		res.status(201);
+		res.end();
+	}
+);
+// BOOKSHOP
 app.post(
 	'/bookshop',
 	basicPopulationLibrary.addBookShop,
@@ -44,23 +61,28 @@ app.post(
 	}
 );
 
+/**
+ * Filtered population
+ */
 app.get('/books', filterQueryLibrary.filterBooks);
 app.get('/book/bookshop/name', filterQueryLibrary.filterBookByBookShopName);
-app.post(
-	'/writer',
-	basicPopulationLibrary.addWriter,
-	(_req: Request, res: Response) => {
-		res.status(201);
-		res.end();
-	}
-);
 
-app.get('/writer', basicPopulationLibrary.getWriter);
+/**
+ * Limited population
+ * Populate by limits using `limit` and `perDocumentLimit` props
+ */
 app.get('/writer/:limit', limitedPopulationLibrary.getWriterWithSpesificBooks);
 app.get(
 	'/writer/:limit/perDoc',
 	limitedPopulationLibrary.getWriterWithSpesificBooksPerDocument
 );
+
+/**
+ * Populate nested/multi-level paths
+ */
+app.get('/nested', nestedLibrary.getNestedUser);
+app.post('/nested', nestedLibrary.createNestedUser);
+app.patch('/nested', nestedLibrary.updateNestedUser);
 // error handler
 app.use(errorHandler);
 
