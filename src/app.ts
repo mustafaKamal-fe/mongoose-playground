6fff4677b8f4deab4e77f77ef0c500b1a1f4d302
mustafaKamal-fe <gistcodee@gmail.com>
2021-07-31 21:53:38 +0300

    cleared branch from other branches files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import errorHandler from './utils/errorHandler';
 import connectMongoose from './utils/connectMongoose';
-import filterQueryLibrary from './middlewares/filters-query/library';
-import basicPopulationLibrary from './middlewares/basic/library';
-import limitedPopulationLibrary from './middlewares/limited/library';
 import nestedLibrary from './middlewares/nested/index';
 
 const app = express();
@@ -14,68 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // connect to DB (default connection)
 app.use(connectMongoose);
-
-/**
- * Basic population
- */
-
-// PERSON
-app.post(
-	'/person',
-	basicPopulationLibrary.addPerson,
-	(_req: Request, res: Response) => {
-		res.status(201);
-		res.end();
-	}
-);
-app.get('/person', basicPopulationLibrary.getPerson);
-
-// BOOK
-app.post(
-	'/book',
-	basicPopulationLibrary.addBook,
-	(_req: Request, res: Response) => {
-		res.status(201);
-		res.end();
-	}
-);
-app.get('/book', basicPopulationLibrary.getBook);
-
-// WRITER
-app.get('/writer', basicPopulationLibrary.getWriter);
-app.post(
-	'/writer',
-	basicPopulationLibrary.addWriter,
-	(_req: Request, res: Response) => {
-		res.status(201);
-		res.end();
-	}
-);
-// BOOKSHOP
-app.post(
-	'/bookshop',
-	basicPopulationLibrary.addBookShop,
-	(_req: Request, res: Response) => {
-		res.status(201);
-		res.end();
-	}
-);
-
-/**
- * Filtered population
- */
-app.get('/books', filterQueryLibrary.filterBooks);
-app.get('/book/bookshop/name', filterQueryLibrary.filterBookByBookShopName);
-
-/**
- * Limited population
- * Populate by limits using `limit` and `perDocumentLimit` props
- */
-app.get('/writer/:limit', limitedPopulationLibrary.getWriterWithSpesificBooks);
-app.get(
-	'/writer/:limit/perDoc',
-	limitedPopulationLibrary.getWriterWithSpesificBooksPerDocument
-);
 
 /**
  * Populate nested/multi-level paths
