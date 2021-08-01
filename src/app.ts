8cd7d956a2fc0d4b697deb8fe1e5de799898b53f
mustafaKamal-fe <gistcodee@gmail.com>
2021-08-02 01:29:41 +0300

    refPath option

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import errorHandler from './utils/errorHandler';
 import connectMongoose from './utils/connectMongoose';
-import library from './middlewares/platformLibrary';
+// import library from './middlewares/platformLibrary';
 
 const app = express();
 const PORT = 3000;
@@ -15,11 +15,15 @@ app.use(connectMongoose);
 // error handler
 app.use(errorHandler);
 
+/**
+ * SEE DISSCUSSION ON HOW TO USE THIS FEATURE [https://github.com/Automattic/mongoose/issues/7967]
+ */
+
 // ADD platform
-app.post('/platform/multi', library.addPlatform);
-app.post('/game/multi', library.addGame);
-app.get('/game/multi', library.getGame);
-app.post('/ratings/multi', library.addRating);
+// app.post('/platform/multi/withref', library.addPlatform);
+// app.post('/game/multi/withref', library.addGame);
+// app.get('/game/multi/withref', library.getGame);
+// app.post('/ratings/multi/withref', library.addRating);
 app.listen(PORT, () => {
 	console.log(`Server Started Listening at port ${PORT}`);
 });
